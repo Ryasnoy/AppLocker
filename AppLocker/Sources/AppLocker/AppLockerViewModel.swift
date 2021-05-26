03c28530cf519d7e97179ab9fc748ddec0aec162
Oleg <ryasnoy.oleg@gmail.com>
2021-05-26 15:23:18 +0300

    Update AppLockerViewModel.swift

--- AppLocker/Sources/AppLocker/AppLockerViewModel.swift
+++ AppLocker/Sources/AppLocker/AppLockerViewModel.swift
@@ -13,10 +13,29 @@ final class AppLockerViewModel: ObservableObject {
     let headerConfiguration: HeaderConfiguration?
     let appLockerConfiguration: AppLockerConfiguration?
     
+    private let keychainer = Keychainer(serviceName: "\(AppLocker.self)")
+    
+    var code: String? {
+        get {
+            return keychainer.string(forKey: Keychainer.codeKey)
+        }
+        set {
+            guard let newValue = newValue else { return }
+            keychainer.set(newValue, forKey: Keychainer.codeKey)
+        }
+    }
+    
+    
     init(headerConfiguration: HeaderConfiguration? = nil,
          appLockerConfiguration: AppLockerConfiguration? = nil) {
         self.headerConfiguration = headerConfiguration
-        self.appLockerConfiguration = appLockerConfiguration
+        self.appLockerConfiguration = appLockerConfiguration    
     }
 
+}
+
+private extension Keychainer {
+    
+    static let codeKey = "codeKey"
+    
 }
