60fb0cf9aca74d26760d3674732730811471a7b7
Oleg <ryasnoy.oleg@gmail.com>
2021-05-26 13:10:38 +0300

    Create Indicators.swift

---
+++ AppLocker/Sources/AppLocker/Views/Indicators.swift
@@ -0,0 +1,26 @@
+//
+//  Indicators.swift
+//  
+//
+//  Created by Oleh Riasnoi on 26.05.2021.
+//
+
+import SwiftUI
+
+struct Indicators: View {
+    
+    let codeLength: Int
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+    
+}
+
+struct Indicators_Preview: PreviewProvider {
+    
+    static var previews: some View {
+        Indicators(codeLength: 6)
+    }
+    
+}
